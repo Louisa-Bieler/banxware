8095397be286ad8a949b57d044725b1a424da9a9
Louisa Lynne Bieler <louisa.lynne.bieler@gmail.com>
2022-08-21 16:22:37 +0200

    basic working line chart

--- banxware/app.py
+++ banxware/app.py
@@ -32,31 +32,49 @@ def make_balance_df(api_url, auth):
     df = pd.DataFrame(balance, index=[0])
     return df
 
-def transform_transactions(transactions):
-    """Returns df grouped by date"""
+def transform_transactions(transactions, balance):
+    """Returns non-cancelled transactions df grouped by date"""
     transactions_dated = transactions.astype({'date': 'datetime64[ns]'})
     transactions_dated['dates'] = pd.to_datetime(transactions_dated['date']).dt.date
-    transactions_grouped = transactions_dated.groupby(['dates']).sum()
+    transactions_dated_not_cancelled = pd.DataFrame(transactions_dated[transactions_dated['status'] != 'CANCELLED'])
+    transactions_grouped =  pd.DataFrame(transactions_dated_not_cancelled.groupby(['dates']).sum().sort_values(by='dates', ascending=False))    
+    balance = int(balance['amount'].values)
+
+    new_array = []
+
+    for record in transactions_grouped['amount'].values:
+        new_record = balance - record
+        new_array.append(new_record)
+        balance = new_record
+    
+    transactions_grouped['running_total'] = new_array
+
     return transactions_grouped
+   
 
 transactions = make_transaction_df(trans_url, auth=auth)
 balance = make_balance_df(balance_url, auth)
+transactions_grouped = transform_transactions(transactions, balance)
 
+print(transactions_grouped.head())
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.line(transactions_grouped, x=transactions_grouped.index.values, y="running_total", hover_data=["amount"], labels={
+                     "running_total": "Running Total (EUR)",
+                     "x": "Date"}
+                 )
 
-#app.layout = html.Div(children=[
- #   html.H1(children='Hello Dash'),
+app.layout = html.Div(children=[
+    html.H1(children='Merchant Foo'),
 
-  #  html.Div(children='''
-   #     Dash: A web application framework for your data.
-    #'''),
+    html.Div(children='''
+        6 months of cashflow records
+    '''),
 
-  #  dcc.Graph(
-   #     id='example-graph',
-    #    figure=fig
-   # )
-#])
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
